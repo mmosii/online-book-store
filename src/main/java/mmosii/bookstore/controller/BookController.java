cc20bd3854ad0601d0d81a90577fae987e4fc856
Mykola Mosii <mykolamosii@gmail.com>
2023-10-20 12:57:40 +0300

    refactored controller

--- src/main/java/mmosii/bookstore/controller/BookController.java
+++ src/main/java/mmosii/bookstore/controller/BookController.java
@@ -1,5 +1,7 @@
 package mmosii.bookstore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
@@ -8,6 +10,8 @@ import mmosii.bookstore.dto.BookDto;
 import mmosii.bookstore.dto.BookSearchParametersDto;
 import mmosii.bookstore.dto.CreateBookRequestDto;
 import mmosii.bookstore.service.BookService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book management", description = "Endpoints for managing books")
 @Validated
 @RequiredArgsConstructor
 @RestController
@@ -28,21 +33,26 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> findAll() {
-        return bookService.findAll();
+    @Operation(summary = "Get all books", description = "Get a list of all available books")
+    public List<BookDto> findAll(@PageableDefault(size = 5) Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a book", description = "Get a book by id, if this id exists")
     public BookDto getBookById(@PathVariable @Positive Long id) {
         return bookService.getBookById(id);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
+    @Operation(summary = "Create a new book", description = "Create a new book")
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a book", description = "Update a book by id, if this id exists")
     public BookDto updateBook(@PathVariable @Positive Long id,
                               @RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.update(id, bookDto);
@@ -50,11 +60,14 @@ public class BookController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable Long id) {
+    @Operation(summary = "Delete a book", description = "Delete a book by id, if this id exists")
+    public void deleteBook(@PathVariable @Positive Long id) {
         bookService.deleteById(id);
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Search book",
+            description = "Get a list of books within specified parameters")
     public List<BookDto> searchBooks(BookSearchParametersDto searchParameters) {
         return bookService.search(searchParameters);
     }
--- src/main/java/mmosii/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/mmosii/bookstore/service/impl/BookServiceImpl.java
@@ -11,6 +11,7 @@ import mmosii.bookstore.model.Book;
 import mmosii.bookstore.repository.book.BookRepository;
 import mmosii.bookstore.repository.book.BookSpecificationBuilder;
 import mmosii.bookstore.service.BookService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -34,8 +35,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
