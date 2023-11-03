41d8d3b44572b3ff1a80f9f0d4da46f62052aec1
Mykola Mosii <mykolamosii@gmail.com>
2023-11-03 11:29:58 +0200

    added @Transactional

--- src/main/java/mmosii/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/mmosii/bookstore/service/impl/BookServiceImpl.java
@@ -18,9 +18,11 @@ import mmosii.bookstore.repository.book.CategoryRepository;
 import mmosii.bookstore.service.BookService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
